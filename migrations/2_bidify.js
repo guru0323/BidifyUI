2437c287c709e155ff47c00b79ac5d6ae21dde98
(Cryptosi) <37211502+Crypto-SI@users.noreply.github.com>
2021-03-23 16:49:14 +0000

    Add files via upload

---
+++ migrations/2_bidify.js
@@ -0,0 +1,19 @@
+const Bidify = artifacts.require("Bidify");
+
+/*
+const ERC20 = artifacts.require("ERC20");
+const ERC721 = artifacts.require("ERC721");
+*/
+
+module.exports = function (deployer) {
+  deployer.deploy(Bidify);
+
+  /*
+  deployer.deploy(ERC20, 0);
+  deployer.deploy(ERC20, 2);
+  deployer.deploy(ERC20, 4);
+  deployer.deploy(ERC20, 8);
+  deployer.deploy(ERC20, 18);
+  deployer.deploy(ERC721, "", "");
+  */
+};
